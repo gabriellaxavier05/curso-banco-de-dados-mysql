0c6adc17deb0ff5c4847bcaeb2acf0aa8bb2033b
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2024-11-29 20:02:23 -0300

    Término do código da aula 06 do curso de MySQL

--- Exercicios/ex_aula06_alterando_a_estrutura_da_tabela.sql
+++ Exercicios/ex_aula06_alterando_a_estrutura_da_tabela.sql
@@ -1,7 +1,7 @@
 use cadastro;
 
 -- para descrever a tabela
-describe pessoas;
+describe pessoas; /* ou desc pessoas; */
 
 -- alterando a tb pesssoas adicionando uma nova coluna
 alter table pessoas
@@ -30,3 +30,46 @@ modify column profissao varchar(20) not null default ' ';
 -- mudando o nome da coluna
 alter table pessoas
 change column profissao prof varchar(20) not null default ' ';
+
+-- renomeando a tabela pessoas para 'gafanhotos'
+alter table pessoas
+rename to gafanhotos;
+
+desc gafanhotos;
+
+-- criando uma nova tabela
+create table if not exists cursos (
+	nome varchar(3) not null unique, -- unique não permitirá 2 registros com o mesmo nome
+    descricao text,
+    carga int unsigned, -- unsigned não permitirá valores com sinal (-)
+    totaulas int unsigned, -- unsigned não permitirá valores com sinal (-)
+    ano year default '2016' -- se o ano não for especificado, começará valendo a partir de 2016
+) default charset = utf8mb4;
+-- o comando acima criará a tabela cursos se ela NÃO existir
+
+desc cursos;
+
+-- alterando a tabela cursos adicionando a coluna idcurso
+alter table cursos
+add column idcurso int first;
+
+-- alterando a tabela cursos definindo a coluna idcurso como chave primária (criando uma constraint)
+alter table cursos
+add primary key (idcurso);
+
+
+-- criando registros para apagar uma tabela
+create table if not exists teste(
+	id int,
+    nome varchar(100),
+    idade int
+);
+
+insert into teste value
+('1', 'Pedro', '22'),
+('2', 'Maria', '12'),
+('3', 'Maricota', '77');
+
+select * from teste;
+
+drop table if exists teste; -- se a tabela teste existir, ela será apagada
