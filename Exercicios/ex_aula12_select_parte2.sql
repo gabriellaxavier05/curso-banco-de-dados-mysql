48cd5c5c34ec7ba298750153027bef1a6a8e4537
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-02-03 23:42:14 -0300

    Aula 12 - Parte 2 - Exemplos DISTINCT + primerios exemplos de funções de agregação

--- Exercicios/ex_aula12_select_parte2.sql
+++ Exercicios/ex_aula12_select_parte2.sql
@@ -6,7 +6,6 @@ select * from cursos
 where nome LIKE 'P%';
 
 
-
 -- WILDCARDS (brincando com a posição do %)
 -- Exibindo todos os registros de 'cursos' que terminam com a letra A:
 select * from cursos
@@ -27,3 +26,39 @@ where nome LIKE 'PH%P';
 -- Exibindo todos os registros de 'cursos' que tenham o nome começando com PH e tendo a letra P em algum lugar depois disso:
 select * from cursos
 where nome LIKE 'PH%P%';
+
+-- Exibindo todos os registros de 'cursos' que começam com PH, terminam com P e tenham algum caractere em seguiga:
+select * from cursos
+where nome LIKE 'PH%P_';
+/*
+Obs.:
+1 _ => 1 caractere
+2 __ => 2 caracteres
+*/
+
+-- Exibindo registros de 'cursos' que começam com PH, tenham 2 caracteres quaisquer depois e depois tenham a letra T
+select * from cursos
+where nome LIKE 'P__T%';
+
+-- Exibindo registros de 'gafanhotos' que tenham 'Silva' em qualquer lugar do nome
+select * from gafanhotos
+where nome LIKE '%Silva%';
+
+-- Exibindo registros de 'gafanhotos' que tenham o sobrenome 'Silva'
+select * from gafanhotos
+where nome LIKE '%_Silva%';
+
+
+-- DISTINCT
+-- Exibindo a nacionalidade das pessoas da tabela 'gafanhotos' sem duplicidade de registros
+select distinct nacionalidade from gafanhotos
+order by nacionalidade;
+
+
+-- AGREGAÇÕES
+-- Contando quantos registros tem a tabela 'cursos':
+select count(*) from cursos;
+
+-- Contando quantos registros na tabela 'cursos' tem a carga maior que 40: 
+select count(*) from cursos
+where carga > 40;
