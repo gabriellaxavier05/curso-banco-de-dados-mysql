9b98e8508d0ecd6e44a0efba55cc4b72b88a77eb
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-02-05 21:22:52 -0300

    Enunciado de exercícios propostos da aula 12

---
+++ Exercicios/exercicios_propostos_aula12.sql
@@ -0,0 +1,15 @@
+/*
+
+Exercícios propostos ao final da aula:
+
+1. Uma lista com todos os nomes das gafanhotas
+2. Uma lista com todos os dados de todos aqueles que nasceram entre 01/01/2000 e 31/12/2015
+3. Uma lista com os dados de todos os homens que trabalham como programadores
+4. Uma lista com os dados de todas as mulheres que nasceram no Brasil e que têm seu nome iniciante com a letra J
+5. Uma lista com o nome e nacionalidade de todos os homens que têm Silva no nome, não nasceram no Brasil e pesam menos de 100 kg
+6. Qual é a maior altura entre gafanhotos homens que moram no Brasil?
+7. Qual é a média de peso dos gafanhotos cadastrados?
+8. Qual é o menor peso entre gafanhotos mulheres que nasceram fora do Brasil e entre 01/01/1990 e 31/12/2000?
+9. Quantas gafanhotos mulheres têm mais de 1.90 de altura?
+
+*/
