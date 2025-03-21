3f643c848bb32792c692228f237831355ae0d5c3
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-03-20 22:09:47 -0300

    Continuação aula Select (parte 3) - Uso do group by com having

--- Exercicios/ex_aula13_select_parte3.sql
+++ Exercicios/ex_aula13_select_parte3.sql
@@ -9,3 +9,29 @@ select carga from cursos
 group by carga;
 
 -- Agrupando e agregando
+select carga, count(nome) from cursos
+group by carga;
+
+-- Contando quantos registros há em um agrupamento:
+select totaulas, count(*) from cursos
+group by totaulas
+order by totaulas;
+ 
+ 
+-- Selecionando todos os registros da tb 'cursos' onde o totaulas é igual a 30
+select * from cursos
+where totaulas = 30;
+-- ou
+select carga, count(nome) from cursos
+where totaulas = 30
+group by carga;
+
+-- Agrupando e agregando 2 (usando HAVING)
+select carga, count(nome) from cursos
+group by carga
+having count(nome) > 3;
+-- Acima, com o having será exibido apenas as cargas horárias que possuem + de 3 cursos.
+
+-- O código abaixo é o mesmo resultado que o código anterior.
+select nome, carga from cursos
+where carga = 40;
